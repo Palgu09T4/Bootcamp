45e47b394767331b59028758e7dc8095c30fdea5
healerfighter <palgunitr@gmail.com>
2025-12-15 20:59:57 +0530

    Initial commit

---
+++ code1.py
@@ -0,0 +1,15 @@
+# Program to find the sum and average of two numbers
+
+# Read input values from the user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Calculate sum
+sum_value = a + b
+
+# Calculate average
+average = sum_value / 2
+
+# Display results
+print("Sum =", sum_value)
+print("Average =", average)
